20a2148e77fe23ec5ca80feee02b575037deda4a
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2024-01-09 17:24:48 +0530

    Update all.py

--- 18CSL76-AIML-Programs-main/all.py
+++ 18CSL76-AIML-Programs-main/all.py
@@ -295,6 +295,68 @@ Predicted Output:
 
 
 
+
+7-
+# Import necessary libraries and modules
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture 
+import pandas as pd
+import numpy as np
+
+# Load Iris dataset
+iris = datasets.load_iris()
+
+# Create a DataFrame 'X' with iris features (sepal length, sepal width, petal length, petal width)
+X = pd.DataFrame(iris.data)
+X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
+
+# Create a DataFrame 'Y' with iris target labels (species)
+Y = pd.DataFrame(iris.target)
+Y.columns = ['Targets']
+
+# Define colors for the species
+colormap = np.array(['red', 'lime', 'black'])
+
+# Real Clustering Visualization
+plt.subplot(1,2,1)
+# Scatter plot for real clusters (species) based on petal length and width
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[Y.Targets])
+plt.title('Real Clustering')
+
+# K-Means Clustering
+model1 = KMeans(n_clusters=3)  # Create KMeans model with 3 clusters
+model1.fit(X)  # Fit KMeans model using the features
+
+plt.subplot(1,2,2)
+# Scatter plot for KMeans clusters based on petal length and width
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model1.labels_], s=40)
+plt.title('K Mean Clustering')
+plt.show()
+
+# EM (Expectation-Maximization) Clustering
+model2 = GaussianMixture(n_components=3)  # Create Gaussian Mixture model with 3 components
+model2.fit(X)  # Fit EM model using the features
+
+plt.subplot(1,2,1)
+# Scatter plot for EM clusters based on petal length and width
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model2.predict(X)], s=40)
+plt.title('EM Clustering')
+plt.show()
+
+# Print actual target labels, K-Means labels, EM predicted labels, and accuracy scores
+print("Actual Target is:\n", iris.target)
+print("K Means:\n",model1.labels_)
+print("EM:\n",model2.predict(X))
+print("Accuracy of KMeans is ",sm.accuracy_score(Y,model1.labels_))
+print("Accuracy of EM is ",sm.accuracy_score(Y, model2.predict(X)))
+
+
+
+
+
+
 8-KNN-
 from sklearn.model_selection import train_test_split 
 from sklearn.neighbors import KNeighborsClassifier 
